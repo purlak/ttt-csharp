a47b5e5dd8b786822707c2e20c8ff76d4f94b71c
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-07-02 10:23:22 -0400

    refactor mock

--- tictactoe.Tests/MockGameRulesInterface.cs
+++ tictactoe.Tests/MockGameRulesInterface.cs
@@ -5,21 +5,10 @@ namespace tictactoe.Tests
     {
         public bool DrawIsCalled;
         public bool WonIsCalled;
-        public bool GameIsOver;
 
         public bool Draw(Board board) => DrawIsCalled = true;
 
-        public bool Over(Board board)
-        {
-            GameRules rules = new GameRules();
-            if (rules.Over(board))
-            {
-                GameIsOver = true;
-                return GameIsOver;
-            }
-            GameIsOver = false;
-            return GameIsOver;
-        }
+        public bool Over(Board board) => new GameRules().Over(board);
 
         public bool Won(Board board) => WonIsCalled = true;
     }
